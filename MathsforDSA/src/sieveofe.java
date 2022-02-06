114fa1e12fe37aa564b0bab90d6be04e01c446f9
lakshaycodesgit <lakshay.gulati.ug20@nsut.ac.in>
2022-02-06 08:27:29 +0530

    new

--- MathsforDSA/src/sieveofe.java
+++ MathsforDSA/src/sieveofe.java
@@ -1,5 +1,21 @@
 public class sieveofe {
     public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        sieve(n,primes);
+    }
 
+    private static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i*i <= n ; i++) {
+            if(primes[i]==false){
+                for (int j = i+i; j <=n; j=j+i) {
+                    primes[j]=true;
+                }
+            }
+        }
+        for (int i = 2; i <=n; i++) {
+            if(primes[i]==false)
+                System.out.print(i+" ");
+        }
     }
 }
